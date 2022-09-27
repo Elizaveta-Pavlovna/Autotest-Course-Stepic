3741b3906d3325d1232de24584d478bfddfde009
Elizaveta-Pavlovna <liza99.liza@mail.ru>
2022-09-27 23:56:23 +0300

    Base Page и Main Page

--- test_main_page.py
+++ test_main_page.py
@@ -6,3 +6,6 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    # команда для запуска через терминал pytest -v --tb=line --language=en test_main_page.py
+
+# Проверяем наличие элемента на странице (метод-проверки)
