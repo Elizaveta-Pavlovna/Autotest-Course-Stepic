4db23d04c36ff96ffdc89dc7d8553b67879026b2
Elizaveta-Pavlovna <liza99.liza@mail.ru>
2022-10-05 12:29:18 +0300

    add exception NoSuchElementException in BasePage

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,23 @@
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage():
     #конструктор — метод, который вызывается, когда мы создаем объект.
     # Конструктор объявляется ключевым словом __init__
-    def __init__(self, browser, url):
+    # timeout=10 - неявное ожидание со значением по-умолчанию 10
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     # open открывает нужную страницу в браузере, используя метод get()
     def open(self):
         self.browser.get(self.url)
+
+    # Здесь будем перехватывать исключение. В него будем передавать два аргумента:
+    # как искать (css, id, xpath и тд) и собственно что искать (строку-селектор).
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
--- test_main_page.py
+++ test_main_page.py
@@ -9,3 +9,9 @@ def test_guest_can_go_to_login_page(browser):
     # команда для запуска через терминал pytest -v --tb=line --language=en test_main_page.py
 
 # Проверяем наличие элемента на странице (метод-проверки)
+
+def test_should_be_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
